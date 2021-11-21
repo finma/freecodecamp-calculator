6054b493246e436353a8f63eb97b9f947e626fd9
finma <firmanmaulana2403@gmail.com>
2021-11-21 09:12:26 +0700

    add logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,177 @@
-import React from "react";
+/* eslint-disable no-eval */
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <div class="container">
-      <div class="result" id="display">
-        <p id="result-box">0</p>
-      </div>
+  const [display, setDisplay] = useState("0");
 
-      <div class="buttons">
-        <div class="button action-btn" id="clear">
-          AC
+  const handleClear = () => {
+    setDisplay("0");
+  };
+
+  const handleButton = (btn) => {
+    if (display === "0") {
+      setDisplay(btn);
+    } else {
+      if (btn === "." && display[display.length - 1] === ".") {
+        return;
+      } else {
+        setDisplay(display.concat(btn));
+      }
+    }
+  };
+
+  const handleBackspace = () => {
+    if (display.length > 1) {
+      setDisplay(display.slice(0, -1));
+    } else {
+      setDisplay("0");
+    }
+  };
+
+  const handleResult = () => {
+    setDisplay(String(eval(display)));
+  };
+
+  return (
+    <div className="container">
+      <div className="calculator">
+        <div className="result" id="display">
+          <p id="result-box">{display}</p>
         </div>
-        <div class="button action-btn" id="">
-          MC
-        </div>
-        <div class="button num-button action-btn" id="">
-          {" "}
-          %
-        </div>
-        <div class="button num-button calc-action-btn" id="divide">
-          {" "}
-          /
-        </div>
-        <div class="button num-button seven" id="seven">
-          7
-        </div>
-        <div class="button num-button eight" id="eight">
-          8
-        </div>
-        <div class="button num-button nine" id="nine">
-          9
-        </div>
-        <div class="button num-button calc-action-btn" id="multiply">
-          *
-        </div>
-        <div class="button num-button four" id="four">
-          4
-        </div>
-        <div class="button num-button five" id="five">
-          5
-        </div>
-        <div class="button num-button six" id="six">
-          6
-        </div>
-        <div class="button num-button calc-action-btn" id="substrack">
-          -
-        </div>
-        <div class="button num-button one" id="one">
-          1
-        </div>
-        <div class="button num-button two" id="two">
-          2
-        </div>
-        <div class="button num-button three" id="three">
-          3
-        </div>
-        <div class="button num-button calc-action-btn" id="add">
-          +
-        </div>
-        <div class="button num-button zero" id="zero">
-          0
-        </div>
-        <div class="button num-button point" id="decimal">
-          .
-        </div>
-        <div class="button calc-action-btn" id="equals">
-          =
+
+        <div className="buttons">
+          <div className="button action-btn" id="clear" onClick={handleClear}>
+            <p>AC</p>
+          </div>
+          <div className="button action-btn" id="" onClick={handleBackspace}>
+            <p>DEL</p>
+          </div>
+          <div className="button num-button action-btn" id="">
+            <p>%</p>
+          </div>
+          <div
+            className="button num-button calc-action-btn"
+            id="divide"
+            onClick={(e) => handleButton(e.target.innerText)}
+          >
+            <p>/</p>
+          </div>
+          <div
+            className="button num-button seven"
+            id="seven"
+            value="7"
+            onClick={(e) => handleButton(e.target.innerText)}
+          >
+            <p>7</p>
+          </div>
+          <div
+            className="button num-button eight"
+            id="eight"
+            onClick={(e) => handleButton(e.target.innerText)}
+          >
+            <p>8</p>
+          </div>
+          <div
+            className="button num-button nine"
+            id="nine"
+            onClick={(e) => handleButton(e.target.innerText)}
+          >
+            <p>9</p>
+          </div>
+          <div
+            className="button num-button calc-action-btn"
+            id="multiply"
+            onClick={(e) => handleButton(e.target.innerText)}
+          >
+            <p>*</p>
+          </div>
+          <div
+            className="button num-button four"
+            id="four"
+            onClick={(e) => handleButton(e.target.innerText)}
+          >
+            <p>4</p>
+          </div>
+          <div
+            className="button num-button five"
+            id="five"
+            onClick={(e) => handleButton(e.target.innerText)}
+          >
+            <p>5</p>
+          </div>
+          <div
+            className="button num-button six"
+            id="six"
+            onClick={(e) => handleButton(e.target.innerText)}
+          >
+            <p>6</p>
+          </div>
+          <div
+            className="button num-button calc-action-btn"
+            id="subtract"
+            onClick={(e) => handleButton(e.target.innerText)}
+          >
+            <p>-</p>
+          </div>
+          <div
+            className="button num-button one"
+            id="one"
+            onClick={(e) => handleButton(e.target.innerText)}
+          >
+            <p>1</p>
+          </div>
+          <div
+            className="button num-button two"
+            id="two"
+            onClick={(e) => handleButton(e.target.innerText)}
+          >
+            <p>2</p>
+          </div>
+          <div
+            className="button num-button three"
+            id="three"
+            onClick={(e) => handleButton(e.target.innerText)}
+          >
+            <p>3</p>
+          </div>
+          <div
+            className="button num-button calc-action-btn"
+            id="add"
+            onClick={(e) => handleButton(e.target.innerText)}
+          >
+            <p>+</p>
+          </div>
+          <div
+            className="button num-button zero"
+            id="zero"
+            onClick={(e) => handleButton(e.target.innerText)}
+          >
+            <p>0</p>
+          </div>
+          <div
+            className="button num-button point"
+            id="decimal"
+            onClick={(e) => handleButton(e.target.innerText)}
+          >
+            <p>.</p>
+          </div>
+          <div
+            className="button calc-action-btn"
+            id="equals"
+            onClick={handleResult}
+          >
+            <p> =</p>
+          </div>
         </div>
       </div>
+      <a
+        href="https://github.com/finma"
+        target="_blank"
+        rel="noreferrer"
+        className="bg-gray-100 mt-4 github"
+      >
+        <img src="/github.png" alt="github" />
+      </a>
     </div>
   );
 };
